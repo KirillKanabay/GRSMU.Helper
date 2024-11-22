040806476f1e457f727a85bb043f42d84815ee9f
KirillKanabay <kirill.kanabay@gmail.com>
2024-11-22 20:45:21 +0300

    Added ng-primary library

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout/layout.component';
 import { NavComponent } from './nav/nav.component';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [LayoutComponent, NavComponent],
   exports: [LayoutComponent],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
 })
 export class LayoutModule {}
