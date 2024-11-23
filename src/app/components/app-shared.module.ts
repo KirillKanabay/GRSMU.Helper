a9ce60269179ed18ba3c77d299fedb5f390b5a53
KirillKanabay <kirill.kanabay@gmail.com>
2024-11-23 16:20:35 +0300

    Added header component

--- src/app/components/app-shared.module.ts
+++ src/app/components/app-shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [NotFoundPageComponent],
-  exports: [NotFoundPageComponent]
+  declarations: [NotFoundPageComponent, HeaderComponent],
+  exports: [NotFoundPageComponent, HeaderComponent]
 })
 export class AppSharedModule { }
--- src/app/modules/schedule/schedule.module.ts
+++ src/app/modules/schedule/schedule.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ScheduleComponent } from './components/schedule/schedule.component';
 import { RouterModule } from '@angular/router';
 import { ScheduleRoutes } from './schedule.route';
+import { AppSharedModule } from "../../components/app-shared.module";
 
 @NgModule({
   declarations: [ScheduleComponent],
-  imports: [RouterModule.forChild(ScheduleRoutes)],
+  imports: [
+    AppSharedModule,
+    RouterModule.forChild(ScheduleRoutes), AppSharedModule],
 })
 export class ScheduleModule {}
